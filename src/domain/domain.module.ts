13336b5a3b19559380fe5151536897746105471b
kunjanrshah <kunjanrshah@gmail.com>
2025-02-25 06:26:07 +0530

    fix: optimize auth guard

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule],
   controllers: [],
   providers: [],
   exports: [],
